d3e4a5ee028e0bfb50b34f9511c0382fc3e868fb
Rohitjangra7370 <rohitjangra7370@gmail.com>
2025-06-03 01:58:58 +0530

    further sorted the gazebo launch file and added path for world into it

--- launch/gazebo_launch.py
+++ launch/gazebo_launch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
@@ -9,23 +9,21 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    
-    use_gazebo = LaunchConfiguration('use_gazebo')
 
+    use_gazebo = LaunchConfiguration('use_gazebo', default='true')
+    pkg_path = get_package_share_directory('line_maze_solver')
+    world_path = os.path.join(pkg_path,'worlds','line_world.world')
+
+    gazebo=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ]),
+        launch_arguments={'world': 'world_path'}.items()   # If you take 'world_path' as a argument, it will not work.
+        )
     return LaunchDescription([
+        
+        gazebo,
 
-        DeclareLaunchArgument(
-            'use_gazebo',
-            default_value='true',
-            description='Start Gazebo simulation'
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-            ]),
-            condition=IfCondition(use_gazebo)
-        ),
 
         Node(
             package='gazebo_ros',
